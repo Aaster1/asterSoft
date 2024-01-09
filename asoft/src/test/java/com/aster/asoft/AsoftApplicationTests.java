5327202deee575c9884964d3e3d3638f4ec45696
Aster1 <gangjinsu4@gmail.com>
2024-01-09 18:28:33 +0900

    01091828

---
+++ asoft/src/test/java/com/aster/asoft/AsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aster.asoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
